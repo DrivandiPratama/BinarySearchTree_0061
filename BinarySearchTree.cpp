293186a96694d5758bbfa8be16f90ae9a6a35329
DrivandiPratama <dripandipratama@gmail.com>
2025-06-26 19:04:27 +0700

    Membuat void preorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -114,3 +114,18 @@ public:
         cout << ptr->info << " "; // parent
         inorder(ptr->rightchild);
     }
+
+    void preorder(node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+            
+        cout << ptr->info << " "; // parent
+        preorder(ptr->leftchild);
+        preorder(ptr->rightchild);
+    }
